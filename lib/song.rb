5aaccc632c1a5eaa37e57ae80f6d9df72cb1b5b5
Andranique Boone <andranique.boone@gmail.com>
2019-04-09 22:35:14 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,40 @@
+class Song
+
+  attr_accessor :name, :artist, :genre
+
+  @@count = 0
+  @@genres = []
+  @@artists = []
+  
+  def initialize(name, artist, genre) 
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@count+1
+    genre >> @@genres
+    artist >> @@artists
+  end 
+
+  def count
+    @@count
+  end 
+
+  def genres
+    @@genres
+  end
+
+  def artists
+    @@artists
+  end
+
+  def artist_count
+
+  end
+
+  def genre_count
+    genre_count = {genre=>count}
+  end
+
+  
+  
+end 
